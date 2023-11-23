94d2b99dcd31ff6a8dcadc54382d37232e752fef
devilandcode <devilandcode@proton.me>
2023-11-23 23:28:47 +0300

    add newMovies method to MovieSevice, modify method get in DataBaseInterface

--- views/pages/home.php
+++ views/pages/home.php
@@ -1,6 +1,7 @@
 <?php
 /**
  * @var \App\Kernel\View\ViewInterface $view;
+ * @var array<\App\Models\Movie> $movies
  */
 ?>
 
@@ -10,9 +11,9 @@
             <h3 class="mt-3" style="color:white;">Movie Novelties</h3>
             <hr style="color:white;">
             <div class="movies">
-
-                    <?php $view->component('movie'); ?>
-
+                <?php foreach ($movies as $movie) { ?>
+                    <?php $view->component('movie', ['movie' => $movie]); ?>
+                <?php } ?>
             </div>
         </div>
     </main>
